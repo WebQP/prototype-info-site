cd664c84c3ac70fb48dc5a30076c5151cc22abd8
WebQP <webqp-fl@yandex.ru>
2023-10-20 08:39:08 +0300

    Update 2023_10_07_203717_create_base_blog_category_localisations_table.php

--- database/migrations/2023_10_07_203717_create_base_blog_category_localisations_table.php
+++ database/migrations/2023_10_07_203717_create_base_blog_category_localisations_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('base_blog_category_localisations', function (Blueprint $table) {
+        Schema::create('base_blog_categories_localisations', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('page_id')->default(0)->comment('ID страницы');
             $table->unsignedInteger('localisation_id')->default(0)->comment('ID локализации');
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('base_blog_category_localisations');
+        Schema::dropIfExists('base_blog_categories_localisations');
     }
 };
