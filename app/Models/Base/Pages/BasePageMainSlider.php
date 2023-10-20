e103ae4a5bf993315b7d17c1435703ce040f34ea
WebQP <webqp-fl@yandex.ru>
2023-10-20 08:38:26 +0300

    Create BasePageMainSlider.php

---
+++ app/Models/Base/Pages/BasePageMainSlider.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Base\Pages;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BasePageMainSlider extends Model
+{
+    use HasFactory;
+}
