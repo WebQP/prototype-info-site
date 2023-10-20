2eb0ad0766e9ceb9aad542cb994960a0f2d9d351
WebQP <webqp-fl@yandex.ru>
2023-10-20 08:38:24 +0300

    Create BasePageLocalisation.php

---
+++ app/Models/Base/Pages/BasePageLocalisation.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Base\Pages;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BasePageLocalisation extends Model
+{
+    use HasFactory;
+    protected $table = 'base_pages_localisations';
+}
