04cf4005127e90d9b29f58a2449f401d722206ab
WebQP <webqp-fl@yandex.ru>
2023-10-20 08:40:00 +0300

    Update sidebar-work.blade.php

--- resources/views/control-panel/layouts/sidebar/sidebar-work.blade.php
+++ resources/views/control-panel/layouts/sidebar/sidebar-work.blade.php
@@ -4,6 +4,24 @@
             <span class="align-middle">AdminKit</span>
         </a>
         <ul class="sidebar-nav">
+
+            <li class="sidebar-header">
+                Магазин
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="/manager/page">
+                    <i class="align-middle" data-feather="square"></i>
+                    <span class="align-middle">
+                        Настройки
+                    </span>
+                </a>
+            </li>
+
+            <li class="sidebar-header">
+                Контент
+            </li>
+
             <li class="sidebar-item">
                 <a href="#blog" data-bs-toggle="collapse" class="sidebar-link collapsed" aria-expanded="false">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users align-middle"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
@@ -34,6 +52,7 @@
                 </a>
             </li>
 
+            <!--
             <li class="sidebar-header">
                 Параметры
             </li>
@@ -55,7 +74,7 @@
                     </span>
                 </a>
             </li>
-
+            -->
         </ul>
     </div>
 </nav>
