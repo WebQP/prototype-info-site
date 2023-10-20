8dc461cb68cf01c7d39d07a8fd21727dd0a4f8f2
WebQP <webqp-fl@yandex.ru>
2023-10-20 08:38:04 +0300

    Create BaseBlogCategoryLocalisation.php

---
+++ app/Models/Base/Blog/BaseBlogCategoryLocalisation.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Base\Blog;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BaseBlogCategoryLocalisation extends Model
+{
+    use HasFactory;
+    protected $table = 'base_blog_categories_localisations';
+}
