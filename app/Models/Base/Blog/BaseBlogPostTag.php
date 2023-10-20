6cacb1a1d3f540137279d315aa722be4182e8115
WebQP <webqp-fl@yandex.ru>
2023-10-20 08:38:18 +0300

    Create BaseBlogPostTag.php

---
+++ app/Models/Base/Blog/BaseBlogPostTag.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Base\Blog;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BaseBlogPostTag extends Model
+{
+    use HasFactory;
+}
